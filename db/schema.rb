140bcfd3639d08d0df658b7bfe28308bb88e4bf3
tomomomoaki <tomomomomorou333@gmail.com>
2020-10-31 15:23:59 +0900

    commentsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_30_090710) do
+ActiveRecord::Schema.define(version: 2020_10_31_061851) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,16 @@ ActiveRecord::Schema.define(version: 2020_10_30_090710) do
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["car_id"], name: "index_comments_on_car_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -80,4 +90,6 @@ ActiveRecord::Schema.define(version: 2020_10_30_090710) do
   add_foreign_key "car_tags", "cars"
   add_foreign_key "car_tags", "tags"
   add_foreign_key "cars", "users"
+  add_foreign_key "comments", "cars"
+  add_foreign_key "comments", "users"
 end
