08df9944a6935962cf997fa7c098e04c14a38780
tomomomoaki <tomomomomorou333@gmail.com>
2020-11-01 16:33:08 +0900

    コメント機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,22 @@
 class CommentsController < ApplicationController
 
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.valid?
+      @comment.save
+      redirect_to car_path(@comment.car)
+    else
+      @car = @comment.car
+      render 'cars/show'
+
+    end
+      
   end
-  
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, car_id: params[:car_id] )
+  end
+
 end
