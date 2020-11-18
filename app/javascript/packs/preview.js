a5e55f9c7d09d4162ab74f6522ddc164e16b3f06
tomomomoaki <tomomomomorou333@gmail.com>
2020-11-19 00:14:39 +0900

    editで画像が表示される設定

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,12 +1,6 @@
 function preview () {
   const ImageList = document.getElementById('image-list');
   const CarImage = document.getElementById('car-image-0');
-
-  const selectFile = document.getElementById('select-file');
-  selectFile.addEventListener('click', () => {
-    CarImage.click();
-  });
-
   let Num = 0;
 
   const createImageHTML = (blob) => {
@@ -38,7 +32,7 @@ function preview () {
     inputHTML.setAttribute('id',`car-image-${Num}`);
     inputHTML.setAttribute('name', 'car[images][]');
     inputHTML.setAttribute('type','file');
-    inputHTML.setAttribute('style','display: none;');
+    //inputHTML.setAttribute('style','display: none;');
 
     const selectHTML = document.createElement('div');
     selectHTML.setAttribute('id', 'select-file-new');
@@ -61,26 +55,35 @@ function preview () {
     selectHTML.addEventListener('click', () => {
       inputHTML.click();
     });
-
     inputHTML.addEventListener('change', (e) => {
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
       createImageHTML(blob)
     });
   }
+  const selectFile = document.getElementById('select-file');
+
+  const beforeCarImages = JSON.parse(document.getElementById('before-car-images').dataset.json);
+  beforeCarImages.forEach( function(image) {
+    createImageHTML(image.url);
+  });
+
+  selectFile.addEventListener('click', () => {
+    CarImage.click();
+  });
 
   window.deleteId = function deleteId(element) {
-    console.log(element);
+    debugger;
     const ID = element.id.slice(-1);
     document.getElementById(`car-image-${ID}`).remove();
     document.getElementById(`image-element-${ID}`).remove();
   }
 
   CarImage.addEventListener('change', (e) => {
-
+    debugger;
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
-    createImageHTML(blob)
+    createImageHTML(blob);
   });
 }
 
