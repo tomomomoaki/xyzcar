2dec0232c20b8aa9478457c3cf519591c9d81c18
tomomomoaki <tomomomomorou333@gmail.com>
2020-11-22 14:45:31 +0900

    戻るボタンの設置

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @cars = @user.cars.order('created_at DESC')
+    @cars = @user.cars.order('created_at DESC').page(params[:page])
   end
 end
